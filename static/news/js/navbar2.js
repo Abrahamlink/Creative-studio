2882d1d3aa6266739588d0c6eab49b6f9e7d028a
Abrahamlink <79006415+Abrahamlink@users.noreply.github.com>
2021-12-18 02:06:30 +0500

    Delete static directory

--- static/news/js/navbar2.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.burger').click(function(event) {
-        $('.burger, .navbar__block').toggleClass('active');
-        $('body').toggleClass('no_scroll');
-    })
-});
