1a186f6f6b994e3c2222ebf05f006c6afa24350d
Berkut <brekutsgaming@mail.ru>
2021-12-30 00:33:27 +0500

    Added news path '/categoty/color_scheme'

--- theatre/apps/category/urls.py
+++ theatre/apps/category/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='main_page'),
     path('contacts', views.contacts, name='contacts'),
     path('contacts/send_letter', views.send_mail_latter, name='send_letter'),
+    path('color_scheme', views.render_colors_and_fonts_from_site, name='scheme')
 ]
