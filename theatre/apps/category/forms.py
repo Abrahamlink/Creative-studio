fe5eefcce50d1ddf6202db44fa566ca2ba788a40
Berkut <brekutsgaming@mail.ru>
2022-01-10 20:25:33 +0500

    Changed labels

--- theatre/apps/category/forms.py
+++ theatre/apps/category/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class EmailLetterForm(forms.Form):
-    user_name = forms.CharField(label='Name', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    from_email = forms.EmailField(label='Email', required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label='Password', required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    subject = forms.CharField(label='Тема', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    user_name = forms.CharField(label='Имя', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    from_email = forms.EmailField(label='Email-адрес', required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Пароль', required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    subject = forms.CharField(label='Тема-письма', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     message = forms.CharField(label='Сообщение', widget=forms.Textarea(attrs={'class': 'form-control'}), required=True)
